efa8bb0052fd51dd260e381d75b3d5cd760fe2d2
ChengXuYuanYY <15170057381@163.com>
2019-03-06 23:44:47 +0800

    'base_practice'

---
+++ python-base/grammar.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/3/6 23:35
+a = 10
+if a < 5:
+    name = input("please enter you name:")
+    print('hello my name is', name)
+else:
+    age = input("please entry you age:")
+    print("i'm ", age)
